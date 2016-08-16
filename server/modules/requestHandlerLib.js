d1dae8e978001573ae80c2564af31b62201cd3ce
Vlad Bezden <vlad.bezden@gmail.com>
2016-08-15 20:55:27 -0400

    Fixed issue with relative path to the app folder.

--- server/modules/requestHandlerLib.js
+++ server/modules/requestHandlerLib.js
@@ -6,9 +6,9 @@ function requestHandler(request, response) {
     console.log(`REQUEST: ${request.url}`);
 
     if (request.url === '/' || request.url === '/index' || request.url === '/index.html') {
-        getFileContent(response, './app/index.html', 'text/html');
+        getFileContent(response, 'app/index.html', 'text/html');
     } else if (request.url === '/favicon.ico') {
-        getFileContent(response, './app/assets/favicon.ico', 'image/png');
+        getFileContent(response, 'app/assets/favicon.ico', 'image/png');
     } else if (/^\/[a-zA-Z0-9\/]*.js$/.test(request.url.toString())) {
         getFileContent(response, request.url.toString().substring(1), 'text/javascript');
     } else if (/^\/[a-zA-Z0-9\/]*.css$/.test(request.url.toString())) {
@@ -22,10 +22,16 @@ function requestHandler(request, response) {
 }
 
 function getFileContent(response, fileName, contentType) {
-    fs.readFile(fileName, function (err, data) {
+    const fullFilePath = `${__dirname}/../../${fileName}`; 
+    
+    console.log(fullFilePath);
+
+    fs.readFile(fullFilePath, function (err, data) {
         if (err) {
+            const errorMessage = `${fileName} Not Found!`;
+            console.log(errorMessage)
             response.writeHead(404, { 'Content-Type': contentType });
-            response.write(`${fileName} Not Found!`);
+            response.write(errorMessage);
         }
         else {
             response.writeHead(200, { 'Content-Type': contentType });
